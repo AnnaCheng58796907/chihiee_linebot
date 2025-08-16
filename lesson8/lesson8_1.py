88cdaa6bb9d1cc1aa85978b4379393b03c324d39
raspAnna <q5j6jp6m3@gmail.com>
2025-08-16 10:12:30 +0800

    gradio
    Signed-off-by: raspAnna <q5j6jp6m3@gmail.com>

---
+++ lesson8/lesson8_1.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+    examples = [["徐國堂","2"],["徐瑞彤","1"]]
+)
+demo.launch()
