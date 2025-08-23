7aa8fcee5bcced253d67e45550db9e5faa09edf1
raspAnna <q5j6jp6m3@gmail.com>
2025-08-23 10:58:15 +0800

    flask
    Signed-off-by: raspAnna <q5j6jp6m3@gmail.com>

--- lesson9/main.py
+++ lesson9/main.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Hello, Flask!</p>"
+
+# 第二頁
+@app.route("/name")
+def my_myName():
+    return "<h1>Anna</h1>"
