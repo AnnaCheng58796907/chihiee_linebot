babedb609ebafee45438e734c643cea7151f6d9b
raspAnna <q5j6jp6m3@gmail.com>
2025-08-23 14:49:16 +0800

    line_bot
    Signed-off-by: raspAnna <q5j6jp6m3@gmail.com>

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template_string, request, jsonify
+from flask import Flask,render_template_string, request, jsonify, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 from google import genai
 from dotenv import load_dotenv
 import os
@@ -6,6 +9,8 @@ import os
 load_dotenv()
 app = Flask(__name__)
 client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 @app.route("/")
 def index():
@@ -79,4 +84,19 @@ def chat():
         return jsonify({"html": html_format})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)    
